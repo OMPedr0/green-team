bdbeb90f41730bef09f31cc5704893c75027acb6
OMPedr0_ <pedrogil5a9@gmail.com>
2023-10-07 13:45:56 +0100

    att

--- green-team/src/app/procura/page.tsx
+++ green-team/src/app/procura/page.tsx
@@ -2,13 +2,13 @@ import React from "react"
 
 
 
-export default function Search() {
+export default function Procura() {
 
 
 
   return (
     <div>
-      <h1>Teste</h1>
+      <h1>Teste 2</h1>
     </div>
   )
 }
