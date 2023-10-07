d4f5d18921dc3641eb5a0b4470cf54403b3815e9
OMPedr0_ <pedrogil5a9@gmail.com>
2023-10-07 14:44:56 +0100

    att

---
+++ green-team/src/api/firebaseConfig.ts
@@ -0,0 +1,21 @@
+import { initializeApp } from 'firebase/app';
+import { getAuth, GoogleAuthProvider } from 'firebase/auth';
+import { getFirestore } from "firebase/firestore";
+
+
+
+
+const app = initializeApp({
+    apiKey: "AIzaSyBvrJRo4C2TH7KvE5hnv78YeFnyWeUzffw",
+    authDomain: "green-team-hackatoon.firebaseapp.com",
+    projectId: "green-team-hackatoon",
+    storageBucket: "green-team-hackatoon.appspot.com",
+    messagingSenderId: "290979485161",
+    appId: "1:290979485161:web:33fb85534ca58d1e582778",
+    measurementId: "G-XQLEVEZDLB"
+});
+
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const provider = new GoogleAuthProvider();
---
+++ green-team/src/app/login/page.tsx
@@ -0,0 +1,15 @@
+import Login from "@/api/login/page"
+import React from "react"
+
+
+
+export default function Procura() {
+
+
+
+  return (
+    <div>
+        <Login/>
+    </div>
+  )
+}
