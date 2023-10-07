b68f928d4b8c940497f993b77f4aba82c1b3e561
oxyzinha <146823768+oxyzinha@users.noreply.github.com>
2023-10-07 15:18:51 +0100

    att

---
+++ green-team/src/app/post/post.tsx
@@ -0,0 +1,14 @@
+import React from "react"
+
+
+
+export default function Post() {
+
+
+
+  return (
+    <div>
+      <h1>Teste 3</h1>
+    </div>
+  )
+}
---
+++ green-team/src/app/user/page.tsx
@@ -0,0 +1,14 @@
+import React from "react"
+
+
+
+export default function User() {
+
+
+
+  return (
+    <div>
+      <h1>Teste 2</h1>
+    </div>
+  )
+}
