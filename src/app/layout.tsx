88f3b5ff6e9bb8169cc57e750d4769c46c28eec9
OMPedr0_ <pedrogil5a9@gmail.com>
2023-10-08 11:46:51 +0100

    att

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,7 @@
 import './globals.css'
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
 
-const inter = Inter({ subsets: ['latin'] })
+
 
 export const metadata: Metadata = {
   title: 'Green Team',
@@ -14,8 +13,8 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en" className='bg-black'>
-      <body className={inter.className}>{children}</body>
+    <html lang="en" >
+      <body className='bg-black'>{children}</body>
     </html>
   )
 }
